8cdc3fccedf3f07fd64c6bb4a4f1b37d6874cda4
sheikhemon <sheikhemon@gmail.com>
2021-09-04 21:22:51 +0600

    fat arrow function

---
+++ playground.js
@@ -0,0 +1,27 @@
+// ES6 fat arrow function 
+
+// Normal Function with 0 parameter
+/* function number() {
+    return 10;
+}
+// Normal Function
+/* function number() {
+    return 10;
+}
+console.log(number()); */
+
+// same topic
+/* let number = () => {
+    return 10;
+}
+console.log(number()); */
+
+// when am i using single parameter or single return statement
+// 1 line of code with fat arrow function
+/* let number = () => 10;
+console.log(number()); */
+
+// another way
+
+let num = () => console.log(10);
+num()
